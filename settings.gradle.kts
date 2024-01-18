4155ff7617f4d52ea7fdc6fbdc226d279b18a030
San Kim <waterfog9580@gmail.com>
2024-01-19 08:24:17 +0900

    [WEAV-19] 프로젝트 구조 설정 (#5)

---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+rootProject.name = "weave-server"
+
+// support
+include(":support:common")
+project(":support:common").projectDir = file("support/common")
+
+include(":support:lock")
+project(":support:lock").projectDir = file("support/lock")
+
+include(":support:logger")
+project(":support:logger").projectDir = file("support/logger")
+
+include(":support:mail")
+project(":support:mail").projectDir = file("support/mail")
+
+// domain
+include(":domain")
+project(":domain").projectDir = file("domain")
+
+// application
+include(":application")
+project(":application").projectDir = file("application")
+
+// infrastructure
+include(":infrastructure:persistence")
+project(":infrastructure:persistence").projectDir = file("infrastructure/persistence")
+
+include(":infrastructure:client")
+project(":infrastructure:client").projectDir = file("infrastructure/client")
+
+// bootstrap
+include(":bootstrap:http")
+project(":bootstrap:http").projectDir = file("bootstrap/http")
