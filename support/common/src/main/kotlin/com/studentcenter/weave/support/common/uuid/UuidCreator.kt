62c9bc03578ccefe043de21aa347491fa0261a19
San Kim <waterfog9580@gmail.com>
2024-01-21 19:43:25 +0900

    [WEAV-31] 🩹 UUID creator v7 type2 로 변경

--- support/common/src/main/kotlin/com/studentcenter/weave/support/common/uuid/UuidCreator.kt
+++ support/common/src/main/kotlin/com/studentcenter/weave/support/common/uuid/UuidCreator.kt
@@ -10,13 +10,13 @@ object UuidCreator {
      * millisecond 단위로 정렬된 uuid 를 생성하며,
      * 동일한 시간에 생성될 경우 단조 증가 counter를 활용해, unique 하게 생성됩니다.
      *
-     * @return 생성된 uuid
+     * @return uuid version 7 type 2
      * @see <a href=https://datatracker.ietf.org/doc/draft-ietf-uuidrev-rfc4122bis>Universally Unique IDentifiers (UUID)</a>
      * @see UuidCreator.getTimeOrdered()
      * @author San Kim
      */
     fun create(): UUID {
-        return UuidCreator.getTimeOrderedEpoch()
+        return UuidCreator.getTimeOrderedEpochPlus1()
     }
 
 }
