59d097bd8ecc21f7e8c48bfee9206a5cc3c1625d
San Kim <waterfog9580@gmail.com>
2024-01-25 17:12:27 +0900

    [WEAV-63] Post로 변경

--- bootstrap/http/src/main/kotlin/com/studentcenter/weave/bootstrap/adapter/api/AuthApi.kt
+++ bootstrap/http/src/main/kotlin/com/studentcenter/weave/bootstrap/adapter/api/AuthApi.kt
@@ -1,30 +1,36 @@
 package com.studentcenter.weave.bootstrap.adapter.api
 
 import com.studentcenter.weave.bootstrap.adapter.dto.RefreshLoginTokenResponse
+import com.studentcenter.weave.bootstrap.adapter.dto.RefreshTokenRequest
+import com.studentcenter.weave.bootstrap.adapter.dto.SocialLoginRequest
 import com.studentcenter.weave.bootstrap.adapter.dto.SocialLoginResponse
 import com.studentcenter.weave.domain.enum.SocialLoginProvider
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 
 @Tag(name = "Auth", description = "Auth API")
 @RequestMapping("/api/auth", produces = ["application/json;charset=utf-8"])
 interface AuthApi {
 
     @Operation(summary = "Social Login")
-    @GetMapping("/login/oauth2/{provider}")
+    @PostMapping("/login/{provider}")
     @ResponseStatus(HttpStatus.OK)
     fun socialLogin(
         @PathVariable provider: SocialLoginProvider,
-        @RequestParam idToken: String
+        @RequestBody idToken: SocialLoginRequest,
     ): SocialLoginResponse
 
     @Operation(summary = "Refresh Login Token")
-    @GetMapping("/login/refresh")
+    @PostMapping("/refresh")
     @ResponseStatus(HttpStatus.OK)
     fun refreshLoginToken(
-        @RequestParam refreshToken: String
+        @RequestBody refreshToken: RefreshTokenRequest,
     ): RefreshLoginTokenResponse
 
 }
