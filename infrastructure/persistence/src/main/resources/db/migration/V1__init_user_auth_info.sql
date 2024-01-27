5e525edab7a36ecf63461382ab1de71bf4e1f420
San Kim <waterfog9580@gmail.com>
2024-01-27 21:25:17 +0900

    [WEAV-65] init user auth info 수정

--- infrastructure/persistence/src/main/resources/db/migration/V1__init_user_auth_info.sql
+++ infrastructure/persistence/src/main/resources/db/migration/V1__init_user_auth_info.sql
@@ -1,9 +1,9 @@
 -- 유저 인증 정보 --
-create table user_auth_info
+create table if not exists user_auth_info
 (
     id                    binary(16)   not null,
     user_id               binary(16)   not null,
-    email                 varchar(255) not null,
+    email                 varchar(255) not null collate utf8mb4_unicode_ci,
     social_login_provider varchar(255) not null,
     registered_at         datetime(6)  not null,
     primary key (id),
