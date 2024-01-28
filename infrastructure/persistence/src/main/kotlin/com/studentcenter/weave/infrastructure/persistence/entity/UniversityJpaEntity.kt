abc3a3170d1cf4984e2db90b1291ca14be33a9fc
doji <doji@channel.io>
2024-01-29 00:53:00 +0900

    [WEAV-107] add flyway and jpa entity

---
+++ infrastructure/persistence/src/main/kotlin/com/studentcenter/weave/infrastructure/persistence/entity/UniversityJpaEntity.kt
@@ -0,0 +1,33 @@
+package com.studentcenter.weave.infrastructure.persistence.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+import java.util.*
+
+@Entity
+@Table(name = "university")
+class UniversityJpaEntity(
+    id: UUID,
+    univId: UUID,
+    name: String,
+    createdAt: LocalDateTime,
+) {
+    @Id
+    var id: UUID = id
+        private set
+
+    @Column(nullable = false)
+    var univId: UUID = univId
+        private set
+
+    @Column(nullable = false)
+    var name: String = name
+        private set
+
+    @Column(nullable = false)
+    var createdAt: LocalDateTime = createdAt
+        private set
+}
