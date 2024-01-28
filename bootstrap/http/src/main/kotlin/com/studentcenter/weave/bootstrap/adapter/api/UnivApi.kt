b197fab0b2e4f043b6d42bcd1b4ee889cfc8499c
doji <doji@channel.io>
2024-01-28 23:30:19 +0900

    [WEAV-68] univ mock api

---
+++ bootstrap/http/src/main/kotlin/com/studentcenter/weave/bootstrap/adapter/api/UnivApi.kt
@@ -0,0 +1,29 @@
+package com.studentcenter.weave.bootstrap.adapter.api
+
+import com.studentcenter.weave.bootstrap.adapter.dto.DomainAddressResponse
+import com.studentcenter.weave.bootstrap.adapter.dto.MajorsResponse
+import com.studentcenter.weave.bootstrap.adapter.dto.UniversitiesResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+
+
+@Tag(name = "Univ", description = "University API")
+@RequestMapping("/api/univ", produces = ["application/json;charset=utf-8"])
+interface UnivApi {
+
+    @Operation(summary = "Find all university")
+    @GetMapping
+    fun findAll(): UniversitiesResponse
+
+    @Operation(summary = "Get all major by university")
+    @GetMapping("/{univName}/majors")
+    fun getAllMajorByUniv(@PathVariable univName: String): MajorsResponse
+
+    @Operation(summary = "Get domain address by university")
+    @GetMapping("/{univName}/domain-address")
+    fun getDomainAddressByUniv(@PathVariable univName: String): DomainAddressResponse
+
+}
