85bbe4defa53466068111ff6d67dae36f7b658ed
San Kim <waterfog9580@gmail.com>
2024-01-28 11:22:10 +0900

    [WEAV-65] 🩹 import 수정

--- bootstrap/http/src/main/kotlin/com/studentcenter/weave/bootstrap/WeaveHttpApplication.kt
+++ bootstrap/http/src/main/kotlin/com/studentcenter/weave/bootstrap/WeaveHttpApplication.kt
@@ -1,18 +1,9 @@
 package com.studentcenter.weave.bootstrap
 
-import com.studentcenter.weave.bootstrap.common.config.WeaveHttpApplicationConfig
-import com.studentcenter.weave.infrastructure.persistence.common.config.PersistenceConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Import
 
 @SpringBootApplication
-@Import(
-    value = [
-        WeaveHttpApplicationConfig::class,
-        PersistenceConfig::class,
-    ]
-)
 class WeaveHttpApplication
 
 fun main(args: Array<String>) {
--- bootstrap/http/src/main/kotlin/com/studentcenter/weave/bootstrap/common/config/WeaveHttpApplicationConfig.kt
+++ bootstrap/http/src/main/kotlin/com/studentcenter/weave/bootstrap/common/config/WeaveHttpApplicationConfig.kt
@@ -1,14 +1,15 @@
 package com.studentcenter.weave.bootstrap.common.config
 
 import com.studentcenter.weave.application.common.config.ApplicationConfig
+import com.studentcenter.weave.infrastructure.persistence.common.config.PersistenceConfig
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
 @Configuration
 @Import(
     value = [
+        PersistenceConfig::class,
         ApplicationConfig::class,
     ]
 )
-class WeaveHttpApplicationConfig {
-}
+class WeaveHttpApplicationConfig
