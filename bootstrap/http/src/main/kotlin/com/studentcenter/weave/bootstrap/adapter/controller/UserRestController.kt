b274c2ae5a31c71034070a1d7a18be8478a9035b
San Kim <waterfog9580@gmail.com>
2024-01-29 19:38:53 +0900

    [WEAV-77] ✨ argument resolver 구현

--- bootstrap/http/src/main/kotlin/com/studentcenter/weave/bootstrap/adapter/controller/UserRestController.kt
+++ bootstrap/http/src/main/kotlin/com/studentcenter/weave/bootstrap/adapter/controller/UserRestController.kt
@@ -1,5 +1,6 @@
 package com.studentcenter.weave.bootstrap.adapter.controller
 
+import com.studentcenter.weave.application.vo.UserTokenClaims
 import com.studentcenter.weave.bootstrap.adapter.api.UserApi
 import com.studentcenter.weave.bootstrap.adapter.dto.RegisterUserRequest
 import com.studentcenter.weave.bootstrap.adapter.dto.RegisterUserResponse
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 class UserRestController : UserApi {
 
     override fun register(
-        authToken: String,
+        registerTokenClaim: UserTokenClaims.RegisterToken,
         request: RegisterUserRequest
     ): RegisterUserResponse {
         return RegisterUserResponse(
