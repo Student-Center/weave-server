1327c3943e66619fb818f84eff4c10cec2ab7f5d
유도진 <djyou128@gmail.com>
2024-01-29 12:22:32 +0900

    [WEAV-107] fix jpa entity & remove sql trailing comma (#28)

--- infrastructure/persistence/src/main/kotlin/com/studentcenter/weave/infrastructure/persistence/entity/MajorJpaEntity.kt
+++ infrastructure/persistence/src/main/kotlin/com/studentcenter/weave/infrastructure/persistence/entity/MajorJpaEntity.kt
@@ -11,33 +11,23 @@ import java.util.*
 @Table(name = "major")
 class MajorJpaEntity(
     id: UUID,
+    univId: UUID,
     name: String,
-    domainAddress: String,
-    logoAddress: String,
     createdAt: LocalDateTime,
-    updatedAt: LocalDateTime,
 ) {
     @Id
     var id: UUID = id
         private set
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
+    var univId: UUID = univId
+        private set
+
+    @Column(nullable = false)
     var name: String = name
-        private set
-
-    @Column(nullable = false, columnDefinition = "varchar(255)")
-    var domainAddress: String = domainAddress
-        private set
-
-    @Column(nullable = false, columnDefinition = "varchar(255)")
-    var logoAddress: String = logoAddress
         private set
 
     @Column(nullable = false)
     var createdAt: LocalDateTime = createdAt
-        private set
-
-    @Column(nullable = false)
-    var updatedAt: LocalDateTime = updatedAt
         private set
 }
