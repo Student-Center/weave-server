08872cfc54a4064ab0a64472523822ae6c57bc10
San Kim <waterfog9580@gmail.com>
2024-01-29 10:50:44 +0900

    [WEAV-108] 테스트 수정

--- application/src/test/kotlin/com/studentcenter/weave/application/service/util/impl/UserTokenServiceImplTest.kt
+++ application/src/test/kotlin/com/studentcenter/weave/application/service/util/impl/UserTokenServiceImplTest.kt
@@ -54,122 +54,109 @@ class UserTokenServiceImplTest : DescribeSpec({
 
     describe("generateRegisterToken") {
 
-        context("회원가입 토큰을 생성한다") {
+        it("회원가입 토큰을 생성한다") {
+            // arrange
+            val email = Email("test@test.com")
+            val nickname = Nickname("test")
+            val provider = SocialLoginProvider.APPLE
 
-            it("회원가입 토큰을 생성한다") {
-                // arrange
-                val email = Email("test@test.com")
-                val nickname = Nickname("test")
-                val provider = SocialLoginProvider.APPLE
+            // act
+            val actual = sut.generateRegisterToken(email, nickname, provider)
 
-                // act
-                val actual = sut.generateRegisterToken(email, nickname, provider)
+            // assert
+            actual.shouldBeInstanceOf<String>()
 
-                // assert
-                actual.shouldBeInstanceOf<String>()
-            }
         }
 
     }
 
     describe("resolveRegisterToken") {
 
-        context("회원가입 토큰을 파싱하여 유저정보가 있는 RegisterToken Claim객체를 반환한다") {
+        it("회원가입 토큰을 파싱하여 유저정보가 있는 RegisterToken Claim객체를 반환한다") {
+            // arrange
+            val email = Email("test@test.com")
+            val nickname = Nickname("test")
+            val provider = SocialLoginProvider.APPLE
+            val registerToken = sut.generateRegisterToken(email, nickname, provider)
 
-            it("회원가입 토큰을 파싱하여 유저정보가 있는 RegisterToken Claim객체를 반환한다") {
-                // arrange
-                val email = Email("test@test.com")
-                val nickname = Nickname("test")
-                val provider = SocialLoginProvider.APPLE
-                val registerToken = sut.generateRegisterToken(email, nickname, provider)
+            // act
+            val actual = sut.resolveRegisterToken(registerToken)
 
-                // act
-                val actual = sut.resolveRegisterToken(registerToken)
-
-                // assert
-                actual shouldBe UserTokenClaims.RegisterToken(
-                    email = email,
-                    nickname = nickname,
-                    socialLoginProvider = provider,
-                )
-            }
+            // assert
+            actual shouldBe UserTokenClaims.RegisterToken(
+                email = email,
+                nickname = nickname,
+                socialLoginProvider = provider,
+            )
         }
+
     }
 
     describe("generateAccessToken") {
 
-        context("유저정보를 토큰에 담아서 AccessToken을 생성한다") {
+        it("유저정보를 토큰에 담아서 AccessToken을 생성한다") {
+            // arrange
+            val user: User = UserFixtureFactory.create()
 
-            it("유저정보를 토큰에 담아서 AccessToken을 생성한다") {
-                // arrange
-                val user: User = UserFixtureFactory.create()
+            // act
+            val actual: String = sut.generateAccessToken(user)
 
-                // act
-                val actual: String = sut.generateAccessToken(user)
-
-                // assert
-                actual.shouldBeInstanceOf<String>()
-            }
+            // assert
+            actual.shouldBeInstanceOf<String>()
         }
+
     }
 
     describe("resolveAccessToken") {
 
-        context("AccessToken을 파싱하여 유저정보가 있는 AccessToken Claim객체를 반환한다") {
+        it("AccessToken을 파싱하여 유저정보가 있는 AccessToken Claim객체를 반환한다") {
+            // arrange
+            val user: User = UserFixtureFactory.create()
+            val accessToken: String = sut.generateAccessToken(user)
 
-            it("AccessToken을 파싱하여 유저정보가 있는 AccessToken Claim객체를 반환한다") {
-                // arrange
-                val user: User = UserFixtureFactory.create()
-                val accessToken: String = sut.generateAccessToken(user)
+            // act
+            val actual: UserTokenClaims.AccessToken = sut.resolveAccessToken(accessToken)
 
-                // act
-                val actual: UserTokenClaims.AccessToken = sut.resolveAccessToken(accessToken)
-
-                // assert
-                actual shouldBe UserTokenClaims.AccessToken(
-                    userId = user.id,
-                    nickname = user.nickname,
-                    email = user.email,
-                    avatar = user.avatar,
-                )
-            }
+            // assert
+            actual shouldBe UserTokenClaims.AccessToken(
+                userId = user.id,
+                nickname = user.nickname,
+                email = user.email,
+                avatar = user.avatar,
+            )
         }
+
     }
 
     describe("generateRefreshToken") {
 
-        context("유저정보를 토큰에 담아서 RefreshToken을 생성한다") {
+        it("유저정보를 토큰에 담아서 RefreshToken을 생성한다") {
+            // arrange
+            val user = UserFixtureFactory.create()
 
-            it("유저정보를 토큰에 담아서 RefreshToken을 생성한다") {
-                // arrange
-                val user = UserFixtureFactory.create()
+            // act
+            val actual = sut.generateRefreshToken(user)
 
-                // act
-                val actual = sut.generateRefreshToken(user)
-
-                // assert
-                actual.shouldBeInstanceOf<String>()
-            }
+            // assert
+            actual.shouldBeInstanceOf<String>()
         }
+
     }
 
     describe("resolveRefreshToken") {
 
-        context("RefreshToken을 파싱하여 유저정보가 있는 RefreshToken Claim객체를 반환한다") {
+        it("RefreshToken을 파싱하여 유저정보가 있는 RefreshToken Claim객체를 반환한다") {
+            // arrange
+            val user = UserFixtureFactory.create()
+            val refreshToken = sut.generateRefreshToken(user)
 
-            it("RefreshToken을 파싱하여 유저정보가 있는 RefreshToken Claim객체를 반환한다") {
-                // arrange
-                val user = UserFixtureFactory.create()
-                val refreshToken = sut.generateRefreshToken(user)
+            // act
+            val actual = sut.resolveRefreshToken(refreshToken)
 
-                // act
-                val actual = sut.resolveRefreshToken(refreshToken)
-
-                // assert
-                actual shouldBe UserTokenClaims.RefreshToken(
-                    userId = user.id,
-                )
-            }
+            // assert
+            actual shouldBe UserTokenClaims.RefreshToken(
+                userId = user.id,
+            )
         }
     }
 
