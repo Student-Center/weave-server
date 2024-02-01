69ae3519d0ec2ca9a3ee550dadb788fd3ae21e77
San Kim <waterfog9580@gmail.com>
2024-02-01 11:44:02 +0900

    [WEAV-128] 회원 정보 조회 API 모의 응답 구성

---
+++ bootstrap/http/src/main/kotlin/com/studentcenter/weave/bootstrap/adapter/dto/UserGetMyProfileResponse.kt
@@ -0,0 +1,33 @@
+package com.studentcenter.weave.bootstrap.adapter.dto
+
+import com.studentcenter.weave.domain.enum.AnimalType
+import com.studentcenter.weave.domain.vo.BirthYear
+import com.studentcenter.weave.domain.vo.Height
+import com.studentcenter.weave.domain.vo.MajorName
+import com.studentcenter.weave.domain.vo.Mbti
+import com.studentcenter.weave.domain.vo.Nickname
+import com.studentcenter.weave.support.common.vo.Url
+import io.swagger.v3.oas.annotations.media.Schema
+import java.util.UUID
+
+@Schema(description = "유저 마이페이지 응답")
+data class UserGetMyProfileResponse (
+    @Schema(description = "유저 아이디")
+    val id: UUID,
+    @Schema(description = "닉네임")
+    val nickname: Nickname,
+    @Schema(description = "생년")
+    val birthYear: BirthYear,
+    @Schema(description = "전공명")
+    val majorName: MajorName,
+    @Schema(description = "프로필 이미지")
+    val avatar: Url?,
+    @Schema(description = "MBTI")
+    val mbti: Mbti,
+    @Schema(description = "닮은 동물")
+    val animalType: AnimalType?,
+    @Schema(description = "키")
+    val height: Height?,
+    @Schema(description = "대학교 이메일 인증 여부")
+    val isUniversityEmailVerified: Boolean,
+)
