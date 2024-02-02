079e5616d35b7727a132340221f08e347618cf3d
San Kim <waterfog9580@gmail.com>
2024-02-02 23:19:25 +0900

    [WEAV-158] 로그아웃 유스케이스 구현

--- application/src/main/kotlin/com/studentcenter/weave/application/common/exception/AuthExceptionType.kt
+++ application/src/main/kotlin/com/studentcenter/weave/application/common/exception/AuthExceptionType.kt
@@ -4,4 +4,5 @@ import com.studentcenter.weave.support.common.exception.CustomExceptionType
 
 enum class AuthExceptionType (override val code: String): CustomExceptionType {
     REFRESH_TOKEN_NOT_FOUND("AUTH-001"),
+    USER_NOT_AUTHENTICATED("AUTH-002"),
 }
--- application/src/main/kotlin/com/studentcenter/weave/application/user/service/application/UserLogoutApplicationService.kt
+++ application/src/main/kotlin/com/studentcenter/weave/application/user/service/application/UserLogoutApplicationService.kt
@@ -1,13 +1,27 @@
 package com.studentcenter.weave.application.user.service.application
 
+import com.studentcenter.weave.application.common.exception.AuthExceptionType
 import com.studentcenter.weave.application.user.port.inbound.UserLogoutUseCase
+import com.studentcenter.weave.application.user.port.outbound.UserRefreshTokenRepository
+import com.studentcenter.weave.application.user.vo.UserAuthentication
+import com.studentcenter.weave.support.common.exception.CustomException
+import com.studentcenter.weave.support.security.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 
 @Service
-class UserLogoutApplicationService: UserLogoutUseCase {
+class UserLogoutApplicationService(
+    private val userRefreshTokenRepository: UserRefreshTokenRepository,
+) : UserLogoutUseCase {
 
     override fun invoke() {
-        // TODO: Implement this method
+        val userAuthentication: UserAuthentication = SecurityContextHolder
+            .getContext<UserAuthentication>()
+            ?.getAuthentication() ?: throw CustomException(
+            AuthExceptionType.USER_NOT_AUTHENTICATED,
+            "회원이 인증되지 않았습니다."
+        )
+
+        userRefreshTokenRepository.deleteByUserId(userAuthentication.userId)
     }
 
 }
