4231cbb08625b0d29099aea266961ea8ab98dfdf
San Kim <waterfog9580@gmail.com>
2024-02-03 11:19:21 +0900

    [WEAV-163] 회원 탈퇴 정보 엔티티 정의

--- domain/src/test/kotlin/com/studentcenter/weave/domain/user/entity/DeletedUserInfoTest.kt
+++ domain/src/test/kotlin/com/studentcenter/weave/domain/user/entity/DeletedUserInfoTest.kt
@@ -1,5 +1,7 @@
 package com.studentcenter.weave.domain.user.entity
 
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
@@ -21,6 +23,23 @@ class DeletedUserInfoTest : FunSpec({
         deletedUserInfo.email shouldBe user.email
         deletedUserInfo.socialLoginProvider shouldBe userAuthInfo.socialLoginProvider
         deletedUserInfo.reason shouldBe reason
+    }
+
+    test("탈퇴 사유가 100자 초과인 경우 생성 실패") {
+        // arrange
+        val userAuthInfo = UserAuthInfoFixtureFactory.create()
+        val reason = "탈퇴 사유".repeat(101)
+
+        // act, assert
+        shouldThrow<IllegalArgumentException> { DeletedUserInfo.create(userAuthInfo, reason) }
+    }
+
+    test("탈퇴 사유가 공백인 경우 생성 성공") {
+        // arrange
+        val userAuthInfo = UserAuthInfoFixtureFactory.create()
+
+        // act
+        shouldNotThrowAny { DeletedUserInfo.create(userAuthInfo) }
     }
 
 })
