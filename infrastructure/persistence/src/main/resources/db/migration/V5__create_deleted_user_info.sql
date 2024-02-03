7fcbd006ebb96766eb3431b3cf32aac960e7b887
San Kim <waterfog9580@gmail.com>
2024-02-03 21:37:39 +0900

    Update infrastructure/persistence/src/main/resources/db/migration/V5__create_deleted_user_info.sql
    Co-authored-by: 유도진 <djyou128@gmail.com>

--- infrastructure/persistence/src/main/resources/db/migration/V5__create_deleted_user_info.sql
+++ infrastructure/persistence/src/main/resources/db/migration/V5__create_deleted_user_info.sql
@@ -4,7 +4,7 @@ create table deleted_user_info
     id                    binary(16)   not null primary key,
     email                 varchar(255) not null,
     social_login_provider varchar(255) not null,
-    reason                varchar(255) not null,
+    reason                varchar(255),
     registered_at         datetime     not null,
     deleted_at            datetime     not null
 );
