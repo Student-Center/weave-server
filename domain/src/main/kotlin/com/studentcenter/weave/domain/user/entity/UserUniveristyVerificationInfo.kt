c919f0d378566b606133bd30e960ce0318289176
doji <doji@channel.io>
2024-02-11 13:27:30 +0900

    [WEAV-190] add user univ verification info

---
+++ domain/src/main/kotlin/com/studentcenter/weave/domain/user/entity/UserUniveristyVerificationInfo.kt
@@ -0,0 +1,29 @@
+package com.studentcenter.weave.domain.user.entity
+
+import com.studentcenter.weave.support.common.uuid.UuidCreator
+import com.studentcenter.weave.support.common.vo.Email
+import java.time.LocalDateTime
+import java.util.*
+
+data class UserUniveristyVerificationInfo(
+    val id: UUID = UuidCreator.create(),
+    val userId: UUID,
+    val universityId: UUID,
+    val universityEmail: Email,
+    val verifiedAt: LocalDateTime,
+) {
+
+    companion object {
+        fun create(
+            user: User,
+            universityEmail: Email,
+        ): UserUniveristyVerificationInfo {
+            return UserUniveristyVerificationInfo(
+                userId = user.id,
+                universityId = user.universityId,
+                universityEmail = universityEmail,
+                verifiedAt = LocalDateTime.now(),
+            )
+        }
+    }
+}
---
+++ infrastructure/persistence/src/main/kotlin/com/studentcenter/weave/infrastructure/persistence/user/entity/UserUniveristyVerificationInfoJpaEntity.kt
@@ -0,0 +1,65 @@
+package com.studentcenter.weave.infrastructure.persistence.user.entity
+
+import com.studentcenter.weave.domain.user.entity.UserUniveristyVerificationInfo
+import com.studentcenter.weave.support.common.vo.Email
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+import java.util.*
+
+@Entity
+@Table(name = "user_university_verification_info")
+class UserUniveristyVerificationInfoJpaEntity(
+    id: UUID,
+    userId: UUID,
+    universityId: UUID,
+    universityEmail: String,
+    verifiedAt: LocalDateTime,
+) {
+
+    @Id
+    var id: UUID = id
+        private set
+
+    @Column(unique = true, nullable = false, updatable = false)
+    var userId: UUID = userId
+        private set
+
+    @Column(nullable = false, updatable = false)
+    var universityId: UUID = universityId
+        private set
+
+    @Column(unique = true, nullable = false, updatable = false)
+    var universityEmail: String = universityEmail
+        private set
+
+    @Column(nullable = false, updatable = false)
+    var verifiedAt: LocalDateTime = verifiedAt
+        private set
+
+    companion object {
+
+        fun UserUniveristyVerificationInfo.toJpaEntity(): UserUniveristyVerificationInfoJpaEntity {
+            return UserUniveristyVerificationInfoJpaEntity(
+                id = id,
+                userId = userId,
+                universityId = universityId,
+                universityEmail = universityEmail.value,
+                verifiedAt = verifiedAt,
+            )
+        }
+
+    }
+
+    fun toDomain(): UserUniveristyVerificationInfo {
+        return UserUniveristyVerificationInfo(
+            id = id,
+            userId = userId,
+            universityId = universityId,
+            universityEmail = Email(universityEmail),
+            verifiedAt = verifiedAt,
+        )
+    }
+}
