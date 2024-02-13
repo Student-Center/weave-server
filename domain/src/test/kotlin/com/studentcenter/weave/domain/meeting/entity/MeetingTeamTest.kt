3fd7bd761a40ff46effd94b97b3cce58289139e7
San Kim <waterfog9580@gmail.com>
2024-02-13 17:12:22 +0900

    [WEAV-176] ♻️ 미팅팀 생성 유스케이스 수정

--- domain/src/test/kotlin/com/studentcenter/weave/domain/meeting/entity/MeetingTeamTest.kt
+++ domain/src/test/kotlin/com/studentcenter/weave/domain/meeting/entity/MeetingTeamTest.kt
@@ -4,6 +4,7 @@ import com.studentcenter.weave.domain.meeting.enums.Location
 import com.studentcenter.weave.domain.meeting.enums.MeetingTeamStatus
 import com.studentcenter.weave.domain.meeting.vo.TeamIntroduce
 import com.studentcenter.weave.domain.user.entity.UserFixtureFactory
+import com.studentcenter.weave.domain.user.enums.Gender
 import com.studentcenter.weave.support.common.uuid.UuidCreator
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
@@ -13,62 +14,14 @@ import java.util.UUID
 
 class MeetingTeamTest : FunSpec({
 
-    (2..4).forEach() { memberCount ->
-        test("팀원의 수는 최소 2명 최대 4명까지 가능하다 : $memberCount 명 - 성공") {
-            // arrange
-            val teamIntroduce = TeamIntroduce("팀 한줄 소개")
-            val leaderUser = UserFixtureFactory.create()
-            val memberUserIds = setOf(UuidCreator.create(), UuidCreator.create())
-            val location = Location.BUSAN
-
-            // act, assert
-            shouldNotThrowAny {
-                MeetingTeam.create(
-                    teamIntroduce = teamIntroduce,
-                    leaderUser = leaderUser,
-                    memberUserIds = memberUserIds,
-                    memberCount = memberCount,
-                    location = location,
-                )
-            }
-        }
-    }
-
-    listOf(1, 5).forEach() { memberCount ->
-        test("팀원의 수는 최소 2명 최대 4명까지 가능하다 : $memberCount 명 - 실패") {
-            // arrange
-            val teamIntroduce = TeamIntroduce("팀 한줄 소개")
-            val leaderUser = UserFixtureFactory.create()
-            val memberUserIds = setOf(UuidCreator.create(), UuidCreator.create())
-            val location = Location.BUSAN
-
-            // act, assert
-            shouldThrow<IllegalArgumentException> {
-                MeetingTeam.create(
-                    teamIntroduce = teamIntroduce,
-                    leaderUser = leaderUser,
-                    memberUserIds = memberUserIds,
-                    memberCount = memberCount,
-                    location = location,
-                )
-            }
-        }
-    }
 
     test("미팅 팀 생성시 WAITING 상태로 생성된다.") {
-        // arrange
-        val teamIntroduce = TeamIntroduce("팀 한줄 소개")
-        val leaderUser = UserFixtureFactory.create()
-        val memberUserIds = emptySet<UUID>()
-        val location = Location.BUSAN
-
-        // act
+        // arrange, act
         val meetingTeam = MeetingTeam.create(
-            teamIntroduce = teamIntroduce,
-            leaderUser = leaderUser,
-            memberUserIds = memberUserIds,
+            teamIntroduce = TeamIntroduce("팀 한줄 소개"),
             memberCount = 2,
-            location = location,
+            location = Location.BUSAN,
+            gender = Gender.MAN,
         )
 
         // assert
