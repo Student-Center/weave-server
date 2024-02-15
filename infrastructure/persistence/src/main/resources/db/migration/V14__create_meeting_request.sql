874432fa41caa1b9049eda47a85a91c56eddcedc
doji <doji@channel.io>
2024-02-16 02:07:36 +0900

    reformat

--- infrastructure/persistence/src/main/resources/db/migration/V14__create_meeting_request.sql
+++ infrastructure/persistence/src/main/resources/db/migration/V14__create_meeting_request.sql
@@ -1,11 +1,11 @@
 create table weave.meeting_request
 (
-    id              binary(16)   not null,
+    id                      binary(16)   not null,
     requestingMeetingTeamId binary(16)   not null,
-    receivingMeetingTeamId binary(16)   not null,
-    status varchar(255) not null,
-    createdAt datetime(6)  not null,
-    respondAt datetime(6),
+    receivingMeetingTeamId  binary(16)   not null,
+    status                  varchar(255) not null,
+    createdAt               datetime(6)  not null,
+    respondAt               datetime(6),
     primary key (id),
     index requesting_meeting_team_id_index (requestingMeetingTeamId),
     index receiving_meeting_team_id_index (receivingMeetingTeamId),
