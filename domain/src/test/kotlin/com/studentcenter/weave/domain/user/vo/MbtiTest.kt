5c5a9003c9efeac91e645900f16d2b80537371ac
San Kim <waterfog9580@gmail.com>
2024-02-17 15:03:51 +0900

    [WEAV-236] 대표 MBTI 계산 로직 작성 (#109)

--- domain/src/test/kotlin/com/studentcenter/weave/domain/user/vo/MbtiTest.kt
+++ domain/src/test/kotlin/com/studentcenter/weave/domain/user/vo/MbtiTest.kt
@@ -1,30 +1,59 @@
 package com.studentcenter.weave.domain.user.vo
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 
 class MbtiTest : FunSpec({
 
-    test("MBTI 형식 테스트 1") {
-        Mbti("EnTJ")
-        Mbti("ISTP")
-        Mbti("ESFJ")
-        Mbti("INFP")
-        Mbti("enFp")
+    context("MBTI 유효성 검사") {
+        test("유효한 MBTI 형식 검사") {
+            Mbti("EnTJ")
+            Mbti("ISTP")
+            Mbti("ESFJ")
+            Mbti("INFP")
+            Mbti("enFp")
+        }
+
+        test("유효하지 않은 MBTI 형식 검사") {
+            runCatching { Mbti("EnT") }.exceptionOrNull()
+                ?.shouldBeInstanceOf<IllegalArgumentException>()
+            runCatching { Mbti("EnTJJ") }.exceptionOrNull()
+                ?.shouldBeInstanceOf<IllegalArgumentException>()
+            runCatching { Mbti("EnT1") }.exceptionOrNull()
+                ?.shouldBeInstanceOf<IllegalArgumentException>()
+            runCatching { Mbti("EnT!") }.exceptionOrNull()
+                ?.shouldBeInstanceOf<IllegalArgumentException>()
+        }
     }
 
-    test("MBTI 형식 테스트 2") {
-        runCatching { Mbti("EnT") }
-            .exceptionOrNull().shouldBeInstanceOf<IllegalArgumentException>()
+    context("대표 MBTI 계산") {
+        test("대표 MBTI 계산 - 다수의 MBTI") {
+            val mbtis = listOf(
+                Mbti("EnTJ"),
+                Mbti("ISTP"),
+                Mbti("ESFJ"),
+                Mbti("INFP"),
+                Mbti("enFp")
+            )
+            Mbti.getDominantMbti(mbtis).value shouldBe "ENFP"
+        }
 
-        runCatching { Mbti("EnTJJ") }
-            .exceptionOrNull().shouldBeInstanceOf<IllegalArgumentException>()
+        test("대표 MBTI 계산 - 3개의 MBTI") {
+            val mbtis = listOf(
+                Mbti("EnTJ"),
+                Mbti("ISTP"),
+                Mbti("ESFJ"),
+            )
+            Mbti.getDominantMbti(mbtis).value shouldBe "ESTJ"
+        }
 
-        runCatching { Mbti("EnT1") }
-            .exceptionOrNull().shouldBeInstanceOf<IllegalArgumentException>()
-
-        runCatching { Mbti("EnT!") }
-            .exceptionOrNull().shouldBeInstanceOf<IllegalArgumentException>()
+        test("대표 MBTI 계산 - 2개의 MBTI") {
+            val mbtis = listOf(
+                Mbti("EnTJ"),
+                Mbti("ISTP"),
+            )
+            Mbti.getDominantMbti(mbtis).value shouldBe "ESTP"
+        }
     }
-
 })
