97ffbc791ff01db51267ba0773a4f96cf95b987a
San Kim <waterfog9580@gmail.com>
2024-02-19 16:25:39 +0900

    [WEAV-231] flyway 버전 이슈 수정

--- infrastructure/persistence/src/main/kotlin/com/studentcenter/weave/infrastructure/persistence/meetingTeam/repository/MeetingTeamMemberSummaryJpaRepository.kt
+++ infrastructure/persistence/src/main/kotlin/com/studentcenter/weave/infrastructure/persistence/meetingTeam/repository/MeetingTeamMemberSummaryJpaRepository.kt
@@ -1,11 +1,12 @@
 package com.studentcenter.weave.infrastructure.persistence.meetingTeam.repository
 
-import com.studentcenter.weave.domain.meetingTeam.entity.MeetingTeamMemberSummary
+import com.studentcenter.weave.infrastructure.persistence.meetingTeam.entity.MeetingTeamMemberSummaryJpaEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
-interface MeetingTeamMemberSummaryJpaRepository : JpaRepository<MeetingTeamMemberSummary, UUID> {
+interface MeetingTeamMemberSummaryJpaRepository :
+    JpaRepository<MeetingTeamMemberSummaryJpaEntity, UUID> {
 
-    fun findByMeetingTeamId(meetingTeamId: UUID): MeetingTeamMemberSummary?
+    fun findByMeetingTeamId(meetingTeamId: UUID): MeetingTeamMemberSummaryJpaEntity?
 
 }
