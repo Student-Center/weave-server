7acc2dfc2e0c28c6a0b96557f08f9f778022e865
San Kim <waterfog9580@gmail.com>
2024-02-19 16:40:23 +0900

    [WEAV-231] flyway 버전 이슈 수정 (#115)

--- infrastructure/persistence/src/main/kotlin/com/studentcenter/weave/infrastructure/persistence/meetingTeam/adapter/MeetingTeamMemberSummaryJpaAdapter.kt
+++ infrastructure/persistence/src/main/kotlin/com/studentcenter/weave/infrastructure/persistence/meetingTeam/adapter/MeetingTeamMemberSummaryJpaAdapter.kt
@@ -3,6 +3,7 @@ package com.studentcenter.weave.infrastructure.persistence.meetingTeam.adapter
 import com.studentcenter.weave.application.meetingTeam.port.outbound.MeetingTeamMemberSummaryRepository
 import com.studentcenter.weave.domain.meetingTeam.entity.MeetingTeamMemberSummary
 import com.studentcenter.weave.infrastructure.persistence.common.exception.PersistenceExceptionType
+import com.studentcenter.weave.infrastructure.persistence.meetingTeam.entity.MeetingTeamMemberSummaryJpaEntity.Companion.toJpaEntity
 import com.studentcenter.weave.infrastructure.persistence.meetingTeam.repository.MeetingTeamMemberSummaryJpaRepository
 import com.studentcenter.weave.support.common.exception.CustomException
 import org.springframework.stereotype.Component
@@ -14,7 +15,7 @@ class MeetingTeamMemberSummaryJpaAdapter(
 ) : MeetingTeamMemberSummaryRepository {
 
     override fun save(meetingTeamMemberSummary: MeetingTeamMemberSummary) {
-        meetingTeamMemberSummaryJpaRepository.save(meetingTeamMemberSummary)
+        meetingTeamMemberSummaryJpaRepository.save(meetingTeamMemberSummary.toJpaEntity())
     }
 
     override fun deleteById(id: UUID) {
@@ -30,11 +31,13 @@ class MeetingTeamMemberSummaryJpaAdapter(
                     message = "MeetingTeamMemberSummary(id=$id)를 찾을 수 없습니다."
                 )
             }
+            .toDomain()
     }
 
     override fun getByMeetingTeamId(meetingTeamId: UUID): MeetingTeamMemberSummary {
         return meetingTeamMemberSummaryJpaRepository
             .findByMeetingTeamId(meetingTeamId)
+            ?.toDomain()
             ?: throw CustomException(
                 type = PersistenceExceptionType.RESOURCE_NOT_FOUND,
                 message = "MeetingTeamMemberSummary(meetingTeamId=$meetingTeamId)를 찾을 수 없습니다."
