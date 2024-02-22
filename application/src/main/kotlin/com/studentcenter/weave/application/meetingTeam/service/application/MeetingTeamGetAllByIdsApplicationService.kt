b5b6b0848e8918472c2feaa5387889c8787f6908
doji <doji@channel.io>
2024-02-23 02:13:19 +0900

    [WEAV-221] university cache

--- application/src/main/kotlin/com/studentcenter/weave/application/meetingTeam/service/application/MeetingTeamGetAllByIdsApplicationService.kt
+++ application/src/main/kotlin/com/studentcenter/weave/application/meetingTeam/service/application/MeetingTeamGetAllByIdsApplicationService.kt
@@ -5,8 +5,13 @@ import com.studentcenter.weave.application.meetingTeam.service.domain.MeetingTea
 import com.studentcenter.weave.application.meetingTeam.vo.MeetingTeamInfo
 import com.studentcenter.weave.application.university.port.inbound.UniversityGetByIdUsecase
 import com.studentcenter.weave.application.user.port.inbound.UserQueryUseCase
+import com.studentcenter.weave.domain.meetingTeam.entity.MeetingTeam
+import com.studentcenter.weave.domain.university.entity.University
+import com.studentcenter.weave.domain.user.entity.User
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.*
+import kotlin.collections.HashMap
 
 @Service
 class MeetingTeamGetAllByIdsApplicationService(
@@ -18,12 +23,13 @@ class MeetingTeamGetAllByIdsApplicationService(
     @Transactional(readOnly = true)
     override fun invoke(command: MeetingTeamGetAllByIdsUseCase.Command): MeetingTeamGetAllByIdsUseCase.Result {
         val meetingTeams = meetingTeamDomainService.getAllByIds(command.ids)
+        val univCache = HashMap<UUID, University>(meetingTeams.size * MeetingTeam.MAX_MEMBER_COUNT)
         val meetingTeamInfos = meetingTeams.map { team ->
             val memberInfos = meetingTeamDomainService
                 .findAllMeetingMembersByMeetingTeamId(team.id)
                 .map {
                     val memberUser = userQueryUseCase.getById(it.userId)
-                    val university = universityGetByIdUsecase.invoke(memberUser.universityId)
+                    val university = getUniversity(univCache, memberUser)
                     MeetingTeamInfo.MemberInfo(
                         id = it.id,
                         user = memberUser,
@@ -39,6 +45,21 @@ class MeetingTeamGetAllByIdsApplicationService(
         }
 
         return MeetingTeamGetAllByIdsUseCase.Result(meetingTeamInfos)
+    }
+
+    /**
+     * 비슷한 학교가 많을 것으로 보여서 한번 조회한 뒤에 캐시처리하여 재활용
+     */
+    private fun getUniversity(
+        universityCache: HashMap<UUID, University>,
+        memberUser: User,
+    ): University {
+        if (universityCache.contains(memberUser.universityId).not()) {
+            val univ = universityGetByIdUsecase.invoke(memberUser.universityId)
+            universityCache[univ.id] = univ
+        }
+        val university = universityCache[memberUser.universityId]!!
+        return university
     }
 
 }
