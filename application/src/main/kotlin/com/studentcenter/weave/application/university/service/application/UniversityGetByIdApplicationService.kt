48f9a3bbca11fad1bcb86b2aa28bf6f4391ead83
doji <doji@channel.io>
2024-02-24 23:01:56 +0900

    [WEAV-221] fixme comment

--- application/src/main/kotlin/com/studentcenter/weave/application/university/service/application/UniversityGetByIdApplicationService.kt
+++ application/src/main/kotlin/com/studentcenter/weave/application/university/service/application/UniversityGetByIdApplicationService.kt
@@ -10,6 +10,7 @@ import java.util.*
 class UniversityGetByIdApplicationService(
     private val universityDomainService: UniversityDomainService,
 ) : UniversityGetByIdUsecase {
+    // FIXME(cache): cache layer 도입 시 사용법에 맞춰 적용하기 + Guava 같은 library 이용해서 만료, 방출 정책 적용
     val univCache = HashMap<UUID, University>(128)
 
     override fun invoke(id: UUID): University {
