98fdf645b7afb6a9b05f5d782d3b88aedfe703c2
wonjunYou <youwjune@gmail.com>
2024-02-25 10:26:17 +0900

    [WEAV-254] trailing commas 추가

--- application/src/main/kotlin/com/studentcenter/weave/application/common/exception/MeetingTeamExceptionType.kt
+++ application/src/main/kotlin/com/studentcenter/weave/application/common/exception/MeetingTeamExceptionType.kt
@@ -5,5 +5,5 @@ import com.studentcenter.weave.support.common.exception.CustomExceptionType
 enum class MeetingTeamExceptionType (override val code: String): CustomExceptionType {
     IS_NOT_TEAM_MEMBER("MEETING-TEAM-001"),
     LEADER_CANNOT_LEAVE_TEAM("MEETING-TEAM-002"),
-    INVITATION_LINK_NOT_FOUND("MEETING-TEAM-003")
+    INVITATION_LINK_NOT_FOUND("MEETING-TEAM-003"),
 }
