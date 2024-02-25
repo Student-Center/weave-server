82cdccac9544642b434cd8b165b6c9819f32dbce
doji <doji@channel.io>
2024-02-26 01:17:17 +0900

    set swagger url

--- bootstrap/http/src/main/kotlin/com/studentcenter/weave/bootstrap/common/config/OpenApiConfig.kt
+++ bootstrap/http/src/main/kotlin/com/studentcenter/weave/bootstrap/common/config/OpenApiConfig.kt
@@ -3,16 +3,24 @@ package com.studentcenter.weave.bootstrap.common.config
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class OpenApiConfig {
+class OpenApiConfig(
+    @Value("\${swagger.server.url}")
+    private val serverUrl: String,
+) {
 
     @Bean
     fun openApi(): OpenAPI {
         return OpenAPI()
             .components(securityComponents())
+            .servers(
+                listOf(Server().apply { url = serverUrl })
+            )
     }
 
     private fun securityComponents(): Components {
