17a0ae1057ebf3d4423a0559a00dea1b49464858
doji <doji@channel.io>
2024-02-27 01:03:56 +0900

    [WEAV-265] test

--- application/src/main/kotlin/com/studentcenter/weave/application/meeting/service/application/FindMyRequestMeetingByReceivingTeamIdApplicationService.kt
+++ application/src/main/kotlin/com/studentcenter/weave/application/meeting/service/application/FindMyRequestMeetingByReceivingTeamIdApplicationService.kt
@@ -2,7 +2,6 @@ package com.studentcenter.weave.application.meeting.service.application
 
 import com.studentcenter.weave.application.common.security.context.getCurrentUserAuthentication
 import com.studentcenter.weave.application.meeting.port.inbound.FindMyRequestMeetingByReceivingTeamIdUseCase
-import com.studentcenter.weave.application.meeting.service.domain.MeetingAttendanceDomainService
 import com.studentcenter.weave.application.meeting.service.domain.MeetingDomainService
 import com.studentcenter.weave.application.meetingTeam.port.inbound.MeetingTeamQueryUseCase
 import com.studentcenter.weave.domain.meeting.entity.Meeting
@@ -18,8 +17,14 @@ class FindMyRequestMeetingByReceivingTeamIdApplicationService(
 
     override fun invoke(receivingTeamId: UUID): Meeting? {
         val userId = getCurrentUserAuthentication().userId
-        val myTeam = meetingTeamQueryUseCase.findByMemberUserId(userId)
-            ?: throw NoSuchElementException("내가 속한 미팅팀이 없어요!")
+        val myTeam = meetingTeamQueryUseCase.findByMemberUserId(userId).let {
+            if (it == null || it.isPublished().not()) {
+                throw NoSuchElementException("내가 속한 공개된 미팅 팀이 없어요!")
+            }
+            it
+        }
+
+
         return meetingDomainService.findByRequestingTeamIdAndReceivingTeamId(
             requestingTeamId = myTeam.id,
             receivingTeamId = receivingTeamId,
---
+++ application/src/test/kotlin/com/studentcenter/weave/application/meeting/service/application/FindByRequestingTeamIdAndReceivingTeamIdApplicationServiceTest.kt
@@ -0,0 +1,104 @@
+package com.studentcenter.weave.application.meeting.service.application
+
+import com.studentcenter.weave.application.common.security.context.UserSecurityContext
+import com.studentcenter.weave.application.meeting.outbound.MeetingRepositorySpy
+import com.studentcenter.weave.application.meeting.service.domain.impl.MeetingDomainServiceImpl
+import com.studentcenter.weave.application.meetingTeam.port.inbound.MeetingTeamQueryUseCase
+import com.studentcenter.weave.application.user.vo.UserAuthenticationFixtureFactory
+import com.studentcenter.weave.domain.meeting.entity.MeetingFixtureFactory
+import com.studentcenter.weave.domain.meetingTeam.entity.MeetingTeam
+import com.studentcenter.weave.domain.meetingTeam.entity.MeetingTeamFixtureFactory
+import com.studentcenter.weave.domain.meetingTeam.enums.MeetingTeamStatus
+import com.studentcenter.weave.domain.user.entity.UserFixtureFactory
+import com.studentcenter.weave.domain.user.enums.Gender
+import com.studentcenter.weave.support.security.context.SecurityContextHolder
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.annotation.DisplayName
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.mockk
+import java.util.*
+
+@DisplayName("GetMeetingAttendancesApplicationServiceTest")
+class FindByRequestingTeamIdAndReceivingTeamIdApplicationServiceTest : DescribeSpec({
+
+    val meetingRepositorySpy = MeetingRepositorySpy()
+    val meetingDomainService = MeetingDomainServiceImpl(
+        meetingRepository = meetingRepositorySpy,
+    )
+    val meetingTeamQueryUseCase = mockk<MeetingTeamQueryUseCase>()
+    val sut = FindMyRequestMeetingByReceivingTeamIdApplicationService(
+        meetingDomainService = meetingDomainService,
+        meetingTeamQueryUseCase = meetingTeamQueryUseCase,
+    )
+
+    val user = UserFixtureFactory.create()
+    val userAuthentication = UserAuthenticationFixtureFactory.create(user)
+
+    beforeEach {
+        SecurityContextHolder.setContext(UserSecurityContext(userAuthentication))
+    }
+
+    afterEach {
+        meetingRepositorySpy.clear()
+        SecurityContextHolder.clearContext()
+        clearAllMocks()
+    }
+
+    describe("미팅 요청 여부 조회 테스트") {
+        context("내가 속한 팀이 없는 경우") {
+            it("예외를 던진다") {
+                // arrange
+                every { meetingTeamQueryUseCase.findByMemberUserId(user.id) } returns null
+
+                // act, assert
+                shouldThrow<NoSuchElementException> { sut.invoke(UUID.randomUUID()) }
+            }
+        }
+
+        context("요청한 미팅이 없는 경우") {
+            it("참여 정보가 조회된다") {
+                // arrange
+                val myTeam = MeetingTeamFixtureFactory.create(status = MeetingTeamStatus.PUBLISHED)
+                every { meetingTeamQueryUseCase.findByMemberUserId(user.id) } returns myTeam
+
+                // act
+                val response = sut.invoke(UUID.randomUUID())
+
+                // assert
+                response shouldBe null
+            }
+        }
+
+        context("요청한 미팅이 있는 경우") {
+            it("참여 정보가 조회된다") {
+                // arrange
+                val myTeam = MeetingTeamFixtureFactory.create(
+                    status = MeetingTeamStatus.PUBLISHED,
+                    gender = Gender.MAN,
+                )
+                val receivingTeam = MeetingTeamFixtureFactory.create(
+                    status = MeetingTeamStatus.PUBLISHED,
+                    gender = Gender.WOMAN,
+                )
+                meetingRepositorySpy.save(
+                    MeetingFixtureFactory.create(
+                        requestingTeamId = myTeam.id,
+                        receivingTeamId = receivingTeam.id,
+                    )
+                )
+                every { meetingTeamQueryUseCase.findByMemberUserId(user.id) } returns myTeam
+
+                // act
+                val response = sut.invoke(receivingTeam.id)
+
+                // assert
+                response shouldNotBe null
+            }
+        }
+    }
+
+})
