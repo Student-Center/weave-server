f53ed1336e3ba79727a7fb78cd4498d916dce141
doji <doji@channel.io>
2024-02-29 19:49:18 +0900

    remove import

--- bootstrap/http/src/main/kotlin/com/studentcenter/weave/bootstrap/meeting/controller/MeetingRestController.kt
+++ bootstrap/http/src/main/kotlin/com/studentcenter/weave/bootstrap/meeting/controller/MeetingRestController.kt
@@ -6,7 +6,6 @@ import com.studentcenter.weave.application.meeting.port.inbound.MeetingAttendanc
 import com.studentcenter.weave.application.meeting.port.inbound.MeetingRequestUseCase
 import com.studentcenter.weave.application.meeting.port.inbound.ScrollPendingMeetingUseCase
 import com.studentcenter.weave.bootstrap.meeting.api.MeetingApi
-import com.studentcenter.weave.bootstrap.meeting.dto.MeetingAttendanceCreateRequest
 import com.studentcenter.weave.bootstrap.meeting.dto.MeetingAttendancesResponse
 import com.studentcenter.weave.bootstrap.meeting.dto.MeetingRequestRequest
 import com.studentcenter.weave.bootstrap.meeting.dto.MeetingResponse
