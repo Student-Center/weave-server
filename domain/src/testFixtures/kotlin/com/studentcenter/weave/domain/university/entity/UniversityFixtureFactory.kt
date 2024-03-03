cfcb614283fd0b6f09cc770a7fac0da5685737c1
doji <doji@channel.io>
2024-03-03 21:45:04 +0900

    fix fixture

--- domain/src/testFixtures/kotlin/com/studentcenter/weave/domain/university/entity/UniversityFixtureFactory.kt
+++ domain/src/testFixtures/kotlin/com/studentcenter/weave/domain/university/entity/UniversityFixtureFactory.kt
@@ -11,6 +11,7 @@ object UniversityFixtureFactory {
     fun create(
         id: UUID = UuidCreator.create(),
         name: UniversityName = UniversityName("위브대학교"),
+        displayName: String = "위브대",
         domainAddress: String = "weave.ac.kr",
         logoAddress: String? = null,
         createdAt: LocalDateTime = LocalDateTime.now(),
@@ -19,6 +20,7 @@ object UniversityFixtureFactory {
         return University(
             id = id,
             name = name,
+            displayName = displayName,
             domainAddress = domainAddress,
             logoAddress = logoAddress,
             createdAt = createdAt,
