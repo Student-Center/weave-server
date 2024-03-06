513abc2aa51223765f0b05cb1d71f423b44cc25c
San Kim <waterfog9580@gmail.com>
2024-03-06 22:42:58 +0900

    [WEAV-137] 토큰 만료 Filter 예외 처리 (#163)

---
+++ bootstrap/http/src/main/kotlin/com/studentcenter/weave/bootstrap/common/security/filter/ExceptionHandlingFilter.kt
@@ -0,0 +1,45 @@
+package com.studentcenter.weave.bootstrap.common.security.filter
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.studentcenter.weave.bootstrap.common.exception.ErrorResponse
+import com.studentcenter.weave.support.common.exception.CustomException
+import com.studentcenter.weave.support.security.jwt.exception.JwtExceptionType
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+class ExceptionHandlingFilter(
+    private val objectMapper: ObjectMapper
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        try {
+            filterChain.doFilter(request, response)
+        } catch (e: CustomException) {
+            when(e.type) {
+                JwtExceptionType.JWT_EXPIRED_EXCEPTION -> handleJwtExpiredException(response)
+                else -> throw e
+            }
+        }
+    }
+
+    private fun handleJwtExpiredException(response: HttpServletResponse) {
+        val errorResponse = ErrorResponse(
+            exceptionCode = JwtExceptionType.JWT_EXPIRED_EXCEPTION.code,
+            message = "토큰이 만료되었습니다.",
+        )
+        response.status = HttpStatus.BAD_REQUEST.value()
+        response.contentType = "${MediaType.APPLICATION_JSON_VALUE};charset=${Charsets.UTF_8.name()}"
+        response.writer.write(objectMapper.writeValueAsString(errorResponse))
+    }
+
+}
