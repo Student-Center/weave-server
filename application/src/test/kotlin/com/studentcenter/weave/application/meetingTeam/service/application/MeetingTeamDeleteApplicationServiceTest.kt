ebac417be4ef647d08bb2fff835c28181c56d821
doji <doji@channel.io>
2024-03-18 01:40:31 +0900

    test implement

--- application/src/test/kotlin/com/studentcenter/weave/application/meetingTeam/service/application/MeetingTeamDeleteApplicationServiceTest.kt
+++ application/src/test/kotlin/com/studentcenter/weave/application/meetingTeam/service/application/MeetingTeamDeleteApplicationServiceTest.kt
@@ -1,5 +1,6 @@
 package com.studentcenter.weave.application.meetingTeam.service.application
 
+import com.studentcenter.weave.application.common.security.context.UserSecurityContext
 import com.studentcenter.weave.application.meeting.port.inbound.CancelAllMeetingUseCase
 import com.studentcenter.weave.application.meetingTeam.outbound.MeetingMemberRepositorySpy
 import com.studentcenter.weave.application.meetingTeam.outbound.MeetingTeamMemberSummaryRepositorySpy
@@ -7,12 +8,17 @@ import com.studentcenter.weave.application.meetingTeam.outbound.MeetingTeamRepos
 import com.studentcenter.weave.application.meetingTeam.port.inbound.MeetingTeamDeleteUseCase
 import com.studentcenter.weave.application.meetingTeam.service.domain.impl.MeetingTeamDomainServiceImpl
 import com.studentcenter.weave.application.user.port.inbound.UserQueryUseCase
+import com.studentcenter.weave.application.user.vo.UserAuthenticationFixtureFactory
 import com.studentcenter.weave.domain.meetingTeam.entity.MeetingMember
 import com.studentcenter.weave.domain.meetingTeam.entity.MeetingTeamFixtureFactory
 import com.studentcenter.weave.domain.meetingTeam.enums.MeetingMemberRole
 import com.studentcenter.weave.domain.user.entity.UserFixtureFactory
+import com.studentcenter.weave.support.common.exception.CustomException
+import com.studentcenter.weave.support.security.context.SecurityContextHolder
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
 import io.mockk.mockk
 import org.springframework.stereotype.Service
 
@@ -34,11 +40,24 @@ class MeetingTeamDeleteApplicationServiceTest : DescribeSpec({
     val sut = MeetingTeamDeleteApplicationService(
         meetingTeamDomainService = meetingTeamDomainService,
         cancelAllMeetingUseCase = cancelMeetingTeamUseCase,
-
     )
 
+    val user = UserFixtureFactory.create()
+    val userAuthentication = UserAuthenticationFixtureFactory.create(user)
+
+    beforeEach {
+        SecurityContextHolder.setContext(UserSecurityContext(userAuthentication))
+    }
+
+    afterEach {
+        meetingTeamRepository.clear()
+        meetingMemberRepository.clear()
+        SecurityContextHolder.clearContext()
+        clearAllMocks()
+    }
+
     describe("미팅 팀 삭제 유스케이스") {
-        it("해당 미팀팀과 미팅 멤버 정보를 삭제한다") {
+        it("해당 미팀팀의 멤버가 아니라면 예외를 던진다.") {
             // arrange
             val meetingTeam = MeetingTeamFixtureFactory.create()
             val user1 = UserFixtureFactory.create()
@@ -50,6 +69,30 @@ class MeetingTeamDeleteApplicationServiceTest : DescribeSpec({
                 MeetingMember.create(meetingTeam.id, user2.id, MeetingMemberRole.MEMBER)
 
             meetingTeamRepository.save(meetingTeam)
+            meetingTeamRepository.putUserToTeamMember(user1.id, meetingTeam.id)
+            meetingTeamRepository.putUserToTeamMember(user2.id, meetingTeam.id)
+            meetingMemberRepository.save(meetingMember1)
+            meetingMemberRepository.save(meetingMember2)
+
+            // act, assert
+            shouldThrow<RuntimeException> {
+                sut.invoke(MeetingTeamDeleteUseCase.Command(meetingTeam.id))
+            }
+        }
+
+        it("해당 미팀팀과 미팅 멤버 정보를 삭제한다") {
+            // arrange
+            val meetingTeam = MeetingTeamFixtureFactory.create()
+            val user2 = UserFixtureFactory.create()
+
+            val meetingMember1 =
+                MeetingMember.create(meetingTeam.id, user.id, MeetingMemberRole.LEADER)
+            val meetingMember2 =
+                MeetingMember.create(meetingTeam.id, user2.id, MeetingMemberRole.MEMBER)
+
+            meetingTeamRepository.save(meetingTeam)
+            meetingTeamRepository.putUserToTeamMember(user.id, meetingTeam.id)
+            meetingTeamRepository.putUserToTeamMember(user2.id, meetingTeam.id)
             meetingMemberRepository.save(meetingMember1)
             meetingMemberRepository.save(meetingMember2)
 
