d2b63d6b2f7015a151f1f458482a362c2055fe98
doji <doji@channel.io>
2024-03-18 01:31:18 +0900

    fix leave/delete

--- application/src/main/kotlin/com/studentcenter/weave/application/meetingTeam/service/application/MeetingTeamDeleteApplicationService.kt
+++ application/src/main/kotlin/com/studentcenter/weave/application/meetingTeam/service/application/MeetingTeamDeleteApplicationService.kt
@@ -1,9 +1,11 @@
 package com.studentcenter.weave.application.meetingTeam.service.application
 
+import com.studentcenter.weave.application.common.exception.MeetingTeamExceptionType
+import com.studentcenter.weave.application.common.security.context.getCurrentUserAuthentication
 import com.studentcenter.weave.application.meeting.port.inbound.CancelAllMeetingUseCase
 import com.studentcenter.weave.application.meetingTeam.port.inbound.MeetingTeamDeleteUseCase
 import com.studentcenter.weave.application.meetingTeam.service.domain.MeetingTeamDomainService
-import com.studentcenter.weave.domain.meetingTeam.enums.MeetingTeamStatus
+import com.studentcenter.weave.support.common.exception.CustomException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -15,12 +17,15 @@ class MeetingTeamDeleteApplicationService(
 
     @Transactional
     override fun invoke(command: MeetingTeamDeleteUseCase.Command) {
-        meetingTeamDomainService.getById(command.id).let {
-            if (it.status == MeetingTeamStatus.PUBLISHED) {
-                cancelAllMeetingUseCase.invoke(CancelAllMeetingUseCase.Command(it.id))
-            }
-            meetingTeamDomainService.deleteById(it.id)
+        val myTeam = meetingTeamDomainService.getByMemberUserId(getCurrentUserAuthentication().userId)
+        if (myTeam.id != command.id) {
+            throw CustomException(MeetingTeamExceptionType.IS_NOT_TEAM_MEMBER, "팀 멤버가 아닙니다.")
         }
+
+        if (myTeam.isPublished()) {
+            cancelAllMeetingUseCase.invoke(CancelAllMeetingUseCase.Command(myTeam.id))
+        }
+        meetingTeamDomainService.deleteById(myTeam.id)
     }
 
 }
