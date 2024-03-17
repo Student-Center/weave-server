684228afe7640b790b0654ecc04abe6eecbb50a8
doji <doji@channel.io>
2024-03-18 01:46:32 +0900

    fix leave logic

--- application/src/main/kotlin/com/studentcenter/weave/application/meetingTeam/service/application/MeetingTeamLeaveApplicationService.kt
+++ application/src/main/kotlin/com/studentcenter/weave/application/meetingTeam/service/application/MeetingTeamLeaveApplicationService.kt
@@ -1,11 +1,9 @@
 package com.studentcenter.weave.application.meetingTeam.service.application
 
-import com.studentcenter.weave.application.common.exception.MeetingTeamExceptionType
 import com.studentcenter.weave.application.common.security.context.getCurrentUserAuthentication
 import com.studentcenter.weave.application.meeting.port.inbound.CancelAllMeetingUseCase
 import com.studentcenter.weave.application.meetingTeam.port.inbound.MeetingTeamLeaveUseCase
 import com.studentcenter.weave.application.meetingTeam.service.domain.MeetingTeamDomainService
-import com.studentcenter.weave.support.common.exception.CustomException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -17,18 +15,13 @@ class MeetingTeamLeaveApplicationService(
 
     @Transactional
     override fun invoke(command: MeetingTeamLeaveUseCase.Command) {
-        val myTeam = meetingTeamDomainService.getById(getCurrentUserAuthentication().userId)
-        if (myTeam.id != command.meetingTeamId) {
-            throw CustomException(MeetingTeamExceptionType.IS_NOT_TEAM_MEMBER, "팀의 멤버가 아닙니다.")
+        meetingTeamDomainService.getById(command.meetingTeamId).let {
+            meetingTeamDomainService.deleteMember(getCurrentUserAuthentication().userId, it.id)
+            if (it.isPublished()) {
+                cancelAllMeetingUseCase.invoke(CancelAllMeetingUseCase.Command(it.id))
+                meetingTeamDomainService.deleteById(it.id)
+            }
         }
-
-        if (myTeam.isPublished()) {
-            cancelAllMeetingUseCase.invoke(CancelAllMeetingUseCase.Command(myTeam.id))
-            meetingTeamDomainService.deleteById(myTeam.id)
-            return
-        }
-
-        meetingTeamDomainService.deleteMember(getCurrentUserAuthentication().userId, myTeam.id)
     }
 
 }
