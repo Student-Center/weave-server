6d466756036e2c3b174458cb653cb4411beac149
San Kim <waterfog9580@gmail.com>
2024-04-03 18:25:28 +0900

    [WEAV-322] 리뷰 반영

--- domain/src/test/kotlin/com/studentcenter/weave/domain/chat/entity/ChatRoomTest.kt
+++ domain/src/test/kotlin/com/studentcenter/weave/domain/chat/entity/ChatRoomTest.kt
@@ -1,6 +1,5 @@
 package com.studentcenter.weave.domain.chat.entity
 
-import com.studentcenter.weave.domain.chat.entity.ChatRoom
 import com.studentcenter.weave.domain.meeting.entity.MeetingFixtureFactory
 import com.studentcenter.weave.domain.meeting.enums.MeetingStatus
 import io.kotest.assertions.throwables.shouldThrow
