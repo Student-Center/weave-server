ae784d7a7dc2f91574e57b571f3e3b4dc818a846
San Kim <waterfog9580@gmail.com>
2024-04-03 18:26:36 +0900

    [WEAV-322] 리뷰 반영

--- application/src/main/kotlin/com/studentcenter/weave/application/chat/vo/ChatRoomDetail.kt
+++ application/src/main/kotlin/com/studentcenter/weave/application/chat/vo/ChatRoomDetail.kt
@@ -18,7 +18,7 @@ data class ChatRoomDetail(
 ) {
 
     data class Team(
-        val id: UUID,
+        val userId: UUID,
         val name: String,
         val members: List<Member>,
     ) {
@@ -66,7 +66,7 @@ data class ChatRoomDetail(
             getUser: GetUser,
         ): Team {
             return Team(
-                id = team.id,
+                userId = team.id,
                 name = team.teamIntroduce.value,
                 members = team.members.map { mapMemberToDetailMember(it.userId, getUser) }
             )
