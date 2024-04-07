299a752df5602f154963fffb7061c3cc4e91e281
dojin <djyou128@gmail.com>
2024-04-08 01:17:00 +0900

    [WEAV-000]refactor send verification number logic (#234)

--- application/src/main/kotlin/com/studentcenter/weave/application/user/service/application/SendVerificationEmailService.kt
+++ application/src/main/kotlin/com/studentcenter/weave/application/user/service/application/SendVerificationEmailService.kt
@@ -10,6 +10,7 @@ import com.studentcenter.weave.application.user.vo.UserUniversityVerificationNum
 import com.studentcenter.weave.support.common.exception.CustomException
 import com.studentcenter.weave.support.common.vo.Email
 import org.springframework.stereotype.Service
+import java.util.UUID
 import kotlin.time.Duration.Companion.minutes
 
 @Service
@@ -20,21 +21,38 @@ class SendVerificationEmailService(
 ): SendVerificationEmail {
 
     override fun invoke(universityEmail: Email) {
+        checkAlreadyVerifiedUniversityEmail(universityEmail)
+        val verificationNumber = getOrGenerateVerificationNumber(
+            getCurrentUserAuthentication().userId,
+            universityEmail,
+        )
+
+        verificationNumberMailer.send(universityEmail, verificationNumber, DEFAULT_DURATION_MINUTE.minutes)
+    }
+
+    private fun checkAlreadyVerifiedUniversityEmail(universityEmail: Email) {
         if (verificationInfoDomainService.existsByEmail(universityEmail)) {
             throw CustomException(
                 type = UniversityVerificationExceptionType.VERIFICATED_EMAIL,
                 message = "이미 인증된 이메일입니다."
             )
         }
+    }
 
-        UserUniversityVerificationNumber.generate().run {
+    private fun getOrGenerateVerificationNumber(userId: UUID, universityEmail: Email): UserUniversityVerificationNumber {
+        return userVerificationNumberRepository.findByUserId(userId)?.let {
+            if (universityEmail == it.first) it.second else null
+        } ?: generateVerificationNumber(userId, universityEmail)
+    }
+
+    private fun generateVerificationNumber(userId: UUID, universityEmail: Email): UserUniversityVerificationNumber {
+        return UserUniversityVerificationNumber.generate().also {
             userVerificationNumberRepository.save(
-                getCurrentUserAuthentication().userId,
+                userId,
                 universityEmail,
-                this,
+                it,
                 DEFAULT_DURATION_MINUTE.minutes.inWholeSeconds
             )
-            verificationNumberMailer.send(universityEmail, this, DEFAULT_DURATION_MINUTE.minutes)
         }
     }
 
